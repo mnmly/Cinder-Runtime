d6d26ae9b36718a1b135e984653cda08d11a2496
mnmly <i.am@mnmly.com>
2017-01-27 21:52:59 -0800

    updated dylib

--- include/runtime_app.h
+++ include/runtime_app.h
@@ -455,9 +455,9 @@ void runtime_app::main( const ci::app::RendererRef &defaultRenderer, const char 
 	interpreter->declare( "#define OBJC_SILENCE_GC_DEPRECATIONS 1" );
 	interpreter->AddIncludePath( blockPath.parent_path().parent_path().string() + "/include/" );
 #if defined(NDEBUG) || defined(_NDEBUG) || defined(RELEASE) || defined(MASTER) || defined(GOLD)
-	interpreter->loadFile( blockPath.parent_path().parent_path().string() + "/lib/macosx/Release/libcinder_d.dylib" );
+	interpreter->loadFile( blockPath.parent_path().parent_path().string() + "/lib/macosx/Release/libcinder.dylib" );
 #else
-	interpreter->loadFile( blockPath.parent_path().parent_path().string() + "/lib/macosx/Debug/libcinder_d.dylib" );
+	interpreter->loadFile( blockPath.parent_path().parent_path().string() + "/lib/macosx/Debug/libcinder.dylib" );
 #endif
 	
 	// add other interpreter options
